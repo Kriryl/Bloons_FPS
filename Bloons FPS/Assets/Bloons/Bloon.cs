58d63d58308c18a6daeabcb481e098f31384db84
Kriryl <60150543+Kriryl@users.noreply.github.com>
2021-09-25 09:30:50 +0200

    bloon script and projectile improvements

---
+++ Bloons FPS/Assets/Bloons/Bloon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloon : MonoBehaviour
+{
+    public virtual void SeekPlayer(float speed)
+    {
+
+    }
+}
